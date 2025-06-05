6d60ea3ecf880f5e1acef1ce638833329153716e
TylerTwoForks <tyler.g.edmonds@gmail.com>
2025-06-05 09:52:54 -0400

    updates to layouts

--- keyboards/hotdox76v2/keymaps/tyler/keymap.c
+++ keyboards/hotdox76v2/keymaps/tyler/keymap.c
@@ -6,6 +6,10 @@
 
 enum custom_keycodes {
     FUNC_TOGGLE = SAFE_RANGE,
+    MAC_COPY, 
+    MAC_PASTE,
+    CTRL_COPY,
+    CTRL_PASTE,
     MAC_END,
     MAC_HOME,
 };
@@ -47,6 +51,7 @@ void keyboard_post_init_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint16_t func_layer_timer;
+    static uint16_t macro_timer;
 
     switch (keycode) {
         case FUNC_TOGGLE:
@@ -63,6 +68,62 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false; // Skip normal processing
+        case CTRL_COPY:
+            if (record->event.pressed) {
+                macro_timer = timer_read(); // Record the press time
+            } else {
+                if (timer_elapsed(macro_timer) < TAPPING_TERM) {
+                    tap_code(KC_C);  // Tap: send normal key
+                }else{
+                    register_code(KC_RIGHT_CTRL);
+                    register_code(KC_C);
+                    unregister_code(KC_C);
+                    unregister_code(KC_RIGHT_CTRL);
+                } 
+            }
+            return false;
+        case CTRL_PASTE:
+            if (record->event.pressed) {
+                macro_timer = timer_read(); // Record the press time
+            } else {
+                if (timer_elapsed(macro_timer) < TAPPING_TERM) {
+                    tap_code(KC_V);  // Tap: send normal key
+                }else{
+                    register_code(KC_RIGHT_CTRL);
+                    register_code(KC_V);
+                    unregister_code(KC_V);
+                    unregister_code(KC_RIGHT_CTRL);
+                } 
+            }
+            return false;
+        case MAC_PASTE:
+            if (record->event.pressed) {
+                macro_timer = timer_read(); // Record the press time
+            } else {
+                if (timer_elapsed(macro_timer) < TAPPING_TERM) {
+                    tap_code(KC_V);  // Tap: send normal key
+                }else{                
+                    register_code(KC_RIGHT_GUI);
+                    register_code(KC_V);
+                    unregister_code(KC_V);
+                    unregister_code(KC_RIGHT_GUI);
+                }
+            }
+            return false;
+        case MAC_COPY:
+            if (record->event.pressed) {
+                func_layer_timer = timer_read(); // Record the press time
+            } else {
+                if (timer_elapsed(macro_timer) < TAPPING_TERM) {
+                    tap_code(KC_C);  // Tap: send normal key
+                }else{  
+                    register_code(KC_RIGHT_GUI);
+                    register_code(KC_C);
+                    unregister_code(KC_C);
+                    unregister_code(KC_RIGHT_GUI);
+                }
+            }   
+            return false;
         case MAC_END:
             if (record->event.pressed) {
                 register_code(KC_LGUI);   // Command down
@@ -86,25 +147,25 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //KeymapCEditor - keymap.c viewer and editor - I use this extension to view the keymaps in a more ui friendly way for quick mental parsing.  
     [_MAC] = LAYOUT_ergodox_pretty(
-        KC_GRV,  KC_1,               KC_2,           KC_3,         KC_4,          KC_5,    KC_6,               KC_PSCR, KC_7,    KC_8,         KC_9,         KC_0,             KC_MINS,               KC_EQL,
-        KC_DEL,  KC_Q,               KC_W,           KC_E,         KC_R,          KC_T,    TO(_LINUX),         TO(1),   KC_Y,    KC_U,         KC_I,         KC_O,             KC_P,                  KC_BSLS,
-        KC_ESC,  LALT_T(KC_A),       LGUI_T(KC_S),   LCTL_T(KC_D), LSFT_T(KC_F),  KC_G,                                         KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), RGUI_T(KC_L), RALT_T(KC_SCLN),   KC_QUOT,
-        KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,           KC_C,         KC_V,          KC_B,    OSL(_FUNC),         KC_N,    KC_N,    KC_M,         KC_COMM,      KC_DOT,           MT(MOD_RCTL, KC_SLSH), KC_RSFT,
-        KC_LCTL, KC_F4,              KC_F5,          KC_LEFT,      KC_RIGHT,                                                                     KC_DOWN,      KC_UP,        KC_LBRC,      KC_RBRC,           TO(_LINUX),
-                                                                                    KC_LALT, KC_LGUI,             KC_RALT, KC_A,
-                                                                                                 KC_PGUP,             MAC_HOME,
-                                                                       KC_BSPC, KC_LGUI, KC_TAB,              MAC_END, KC_ENT, KC_SPC
+        KC_GRV,  KC_1,               KC_2,           KC_3,         KC_4,          KC_5,    KC_6,               KC_PSCR,          KC_7,    KC_8,         KC_9,         KC_0,             KC_MINS,               KC_EQL,
+        KC_DEL,  KC_Q,               KC_W,           KC_E,         KC_R,          KC_T,    KC_LEFT_BRACKET,    KC_RIGHT_BRACKET, KC_Y,    KC_U,         KC_I,         KC_O,             KC_P,                  KC_BSLS,
+        KC_ESC,  LALT_T(KC_A),       LGUI_T(KC_S),   LCTL_T(KC_D), LSFT_T(KC_F),  KC_G,                                                  KC_H,    RSFT_T(KC_J), RCTL_T(KC_K), RGUI_T(KC_L), RALT_T(KC_SCLN),   KC_QUOT,
+        KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,           MAC_COPY,     MAC_PASTE,     KC_B,    OSL(_FUNC),         KC_N,             KC_N,    KC_M,         KC_COMM,      KC_DOT,           MT(MOD_RCTL, KC_SLSH), KC_RSFT,
+        KC_LCTL, KC_F4,              KC_F5,          KC_LEFT,      KC_RIGHT,                                                                              KC_DOWN,      KC_UP,        KC_LBRC,      TO(_LINUX),           TO(_LINUX),
+                                                                                                        KC_LALT, KC_LGUI,             KC_RALT, KC_A,
+                                                                                                                     KC_PGUP,             MAC_HOME,
+                                                                                             KC_BSPC, KC_ENT, KC_TAB,             MAC_END, KC_ENT, KC_SPC
     ),
 
     [_LINUX] = LAYOUT_ergodox_pretty(
-        KC_GRV,  KC_1,              KC_2,         KC_3,         KC_4,          KC_5,    KC_6,               KC_PSCR, KC_7,    KC_8,         KC_9,         KC_0,             KC_MINS,               KC_EQL,
-        KC_DEL,  KC_Q,              KC_W,         KC_E,         KC_R,          KC_T,    TO(_FUNC),         TO(1),   KC_Y,    KC_U,         KC_I,         KC_O,             KC_P,                  KC_BSLS,
-        KC_ESC,  LGUI_T(KC_A),      LCTL_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F),  KC_G,                                         KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RGUI_T(KC_SCLN),   KC_QUOT,
-        KC_LSFT, MT(MOD_LCTL, KC_Z),KC_X,         KC_C,         KC_V,          KC_B,    OSL(_FUNC),         KC_N,    KC_N,    KC_M,         KC_COMM,      KC_DOT,           MT(MOD_RCTL, KC_SLSH), KC_RSFT,
-        KC_LCTL, KC_F4,             KC_F5,        KC_LEFT,      KC_RIGHT,                                                                     KC_DOWN,      KC_UP,        KC_LBRC,      KC_RBRC,           TO(_MAC),
-                                                                                    KC_LALT, KC_LGUI,             KC_RALT, KC_A,
-                                                                                                 KC_PGUP,             KC_HOME,
-                                                                        KC_BSPC, KC_LGUI, KC_TAB,             KC_END, KC_ENT, KC_SPC
+        KC_GRV,  KC_1,              KC_2,         KC_3,         KC_4,          KC_5,    KC_6,               KC_PSCR,           KC_7,    KC_8,         KC_9,         KC_0,             KC_MINS,               KC_EQL,
+        KC_DEL,  KC_Q,              KC_W,         KC_E,         KC_R,          KC_T,    KC_LEFT_BRACKET,    KC_RIGHT_BRACKET,  KC_Y,    KC_U,         KC_I,         KC_O,             KC_P,                  KC_BSLS,
+        KC_ESC,  LGUI_T(KC_A),      LCTL_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F),  KC_G,                                                   KC_H,    RSFT_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RGUI_T(KC_SCLN),   KC_QUOT,
+        KC_LSFT, MT(MOD_LCTL, KC_Z),KC_X,         CTRL_COPY,    CTRL_PASTE,          KC_B,    OSL(_FUNC),         KC_N,              KC_N,    KC_M,         KC_COMM,      KC_DOT,           MT(MOD_RCTL, KC_SLSH), KC_RSFT,
+        KC_LCTL, KC_F4,             KC_F5,        KC_LEFT,      KC_RIGHT,                                                                               KC_DOWN,      KC_UP,        TO(_FUNC),      TO(_FUNC),           TO(_MAC),
+                                                                                                        KC_LALT, KC_LGUI,            KC_RALT, KC_A,
+                                                                                                                     KC_PGUP,            KC_HOME,
+                                                                                            KC_BSPC, KC_ENT, KC_TAB,             KC_END, KC_ENT, KC_SPC
     ),
 
     [_FUNC] = LAYOUT_ergodox_pretty(
@@ -112,10 +173,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DEL,  KC_Q,               KC_W,    KC_E,    KC_R,     KC_T,    TO(_OTHER),          TO(1),   KC_Y,    KC_U,    KC_LBRC, KC_RBRC,     KC_P,                    KC_BSLS,
         KC_ESC,  MT(MOD_LGUI,KC_A),               KC_S,    KC_D,    KC_F,     KC_G,                             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_SCLN,             KC_QUOT,
         KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,    KC_C,    KC_V,     KC_B,    MO(_FUNC),           KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,      MT(MOD_RCTL, KC_SLSH),   KC_RSFT,
-        KC_LCTL, KC_F4,              KC_F5,   KC_LEFT, KC_RIGHT,                                                                 KC_DOWN, KC_UP,   KC_LBRC, KC_RBRC,             KC_RGUI,
+        KC_LCTL, KC_F4,              KC_F5,   KC_LEFT, KC_RIGHT,                                                                 KC_DOWN, KC_UP,   KC_LBRC, TO(_OTHER),             KC_RGUI,
                                                                                      KC_LALT, KC_LGUI,             KC_RALT, KC_A,
                                                                                                   KC_PGUP,             KC_PGDN,
-                                                                        KC_BSPC, KC_LGUI, TO(_OTHER),              KC_RCTL, KC_ENT, KC_SPC
+                                                                        KC_BSPC, KC_ENT, TO(_OTHER),              KC_RCTL, KC_ENT, KC_SPC
     ),
 
     [_OTHER] = LAYOUT_ergodox_pretty(
@@ -123,9 +184,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DEL,  KC_Q,               KC_W,    KC_E,    KC_R,     KC_T,    TO(_MAC),           TO(1),   KC_Y,    KC_7,    KC_8,    KC_9,       KC_P,                  KC_BSLS,
         KC_ESC,  KC_A,               KC_S,    KC_D,    KC_F,     KC_G,                                         KC_H,    KC_4,    KC_5,    KC_6,   KC_SCLN,           KC_QUOT,
         KC_LSFT, MT(MOD_LCTL, KC_Z), KC_X,    KC_C,    KC_V,     KC_B,    MO(_FUNC),          KC_N,    KC_N,    KC_1,    KC_2,    KC_3,       MT(MOD_RCTL, KC_SLSH), KC_RSFT,
-        KC_LCTL, KC_F4,              KC_F5,   KC_LEFT, KC_RIGHT,                                                                KC_DOWN, KC_UP,   KC_0,   KC_RBRC,           KC_RGUI,
+        KC_LCTL, KC_F4,              KC_F5,   KC_LEFT, KC_RIGHT,                                                                KC_DOWN, KC_UP,   KC_0,   TO(_MAC),           KC_RGUI,
                                                                                      KC_LALT, KC_LGUI,            KC_RALT, KC_A,
                                                                                                   KC_PGUP,            KC_PGDN,
-                                                                        KC_BSPC, KC_LGUI, KC_TAB,             KC_RCTL, KC_ENT, KC_SPC
+                                                                        KC_BSPC, KC_ENT, KC_TAB,             KC_RCTL, KC_ENT, KC_SPC
     ),
 };
